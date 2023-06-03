4399f5fbd0a28e39150b1b0efe9f372fe16e6af2
Niki Akbar <nikiakbar@gmail.com>
2023-06-03 22:51:35 +0700

    Updated

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,22 @@
-import subprocess, logging
+import boto3
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-
-def run_command(command):
-    command_list = command.split(' ')
-
-    try:
-        logger.info("Running shell command: \"{}\"".format(command))
-        result = subprocess.run(command_list, stdout=subprocess.PIPE);
-        logger.info("Command output:\n---\n{}\n---".format(result.stdout.decode('UTF-8')))
-    except Exception as e:
-        logger.error("Exception: {}".format(e))
-        return False
-
-    return True
-	
 def lambda_handler(event, context):
-	run_command('aws --version && ls -lah' )
+    # Extract the instance ID from the event
+    instance_id = event['instance_id']
+
+    # Create a Boto3 client for EC2
+    ec2_client = boto3.client('ec2')
+
+    # Stop the EC2 instance
+    response = ec2_client.stop_instances(InstanceIds=[instance_id])
+
+    # Return a response
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        message = f"EC2 instance {instance_id} has been stopped successfully."
+    else:
+        message = f"Failed to stop EC2 instance {instance_id}."
+
+    return {
+        'statusCode': 200,
+        'body': message
+    }
